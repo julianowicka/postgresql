0b288f692a692a9b508d3b17ee5f67842442386e
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-09 10:47:51 +0200

    Add script - produce a list of all members who have used a tennis court

--- joinsAndSubqueries.sql
+++ joinsAndSubqueries.sql
@@ -24,3 +24,21 @@ select distinct a.firstname, a.surname from cd.members a
 inner join cd.members b
 on a.memid = b.recommendedby
 order by a.surname, a.firstname
+
+/*Produce a list of all members, along with their recommender*/
+select *  from cd.members a
+left outer join cd.members b
+on
+b.memid = a.recommendedby
+order by a.surname, a.firstname
+
+/*Produce a list of all members who have used a tennis court*/
+select * from cd.bookings b
+inner join cd.facilities f
+on
+b.facid = f.facid
+inner join cd.members m
+on
+b.memid = m.memid
+where
+b.bookid=1
