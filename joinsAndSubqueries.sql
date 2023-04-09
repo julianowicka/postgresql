c946086e4caa627a8dca73d2d98a9518dbf26fd4
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-09 10:45:07 +0200

    Add script - work out the start times of bookings for tennis courts

--- joinsAndSubqueries.sql
+++ joinsAndSubqueries.sql
@@ -8,3 +8,14 @@ surname = 'Farrell'
 and
 firstname = 'David'
 
+/*Work out the start times of bookings for tennis courts*/
+select starttime, name from
+cd.bookings inner join cd.facilities
+on cd.bookings.facid = cd.facilities.facid
+where
+name like 'Tennis%'
+and
+starttime >= '2012-09-21 00:00:00'
+and
+starttime < '2012-09-22 00:00:00'
+
