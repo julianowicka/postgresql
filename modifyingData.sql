0ecc084f9f03389938c76e1623685a8c43831d12
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-11 18:28:33 +0200

    Add script - insert some data into a table

---
+++ modifyingData.sql
@@ -0,0 +1,4 @@
+/*Insert some data into a table*/
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    values (9, 'Spa', 20, 30, 100000, 800);
