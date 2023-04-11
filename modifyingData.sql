6696ba289232980102f9d7c009f55fecf3083c2c
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-11 18:29:41 +0200

    Add script - insert multiple rows of data into a table

--- modifyingData.sql
+++ modifyingData.sql
@@ -2,3 +2,10 @@
 insert into cd.facilities
     (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
     values (9, 'Spa', 20, 30, 100000, 800);
+
+/*Insert multiple rows of data into a table*/
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    values
+        (9, 'Spa', 20, 30, 100000, 800),
+        (10, 'Squash Court 2', 3.5, 17.5, 5000, 80);
