7ee2ca07629e6fe4887fd4835217544a3e5e4595
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-11 18:31:41 +0200

    Add script - insert calculated data into a table

--- modifyingData.sql
+++ modifyingData.sql
@@ -9,3 +9,8 @@ insert into cd.facilities
     values
         (9, 'Spa', 20, 30, 100000, 800),
         (10, 'Squash Court 2', 3.5, 17.5, 5000, 80);
+
+/*Insert calculated data into a table*/
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    select (select max(facid) from cd.facilities)+1, 'Spa', 20, 30, 100000, 800;
